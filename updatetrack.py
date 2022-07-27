7ff6fca261b886c96dc2327f185123f4e78fb26f
2213yash <2213yash@gmail.com>
2022-07-27 08:31:05 -0700

    m

--- updatetrack.py
+++ updatetrack.py
@@ -1,33 +1,35 @@
-# import time
+import time
 import requests
 
-from apscheduler.schedulers.blocking import BlockingScheduler
-sched = BlockingScheduler()
-
-@sched.scheduled_job('interval', minutes=60)
 def trackEveryShipment():
 
     data = requests.get('https://skykingtrack.herokuapp.com/getships')
-    dataJson = data.json()
-    for ships in dataJson:
-        tcode = ships['fields']['tcode']
-        url = f'https://live.skyking.co/api/Track/ConsignmentMTrack_WebSite_AuthNew?cnote={tcode}&key=skm&email=skm@flyking.co.in'
-        headers = {
-            "KeyValue": "wdFm5gZ7Wyh9aLvxcPU5Q1k5vhL/9K0g01d2H4JmJRR8+7VhRoucbv/SkTkRrXnk5R7PJkXm+ZoEtaXaR0Ci+MZ/gCektrIgalRYsorAspNDDISVSsm9qtJtfEraDbwf",
-            "Content-Type": "application/json"
-        }
-        res = requests.get(url, headers=headers)
-        status = res.json()
-        if(len(status) == 0):
-            qstatus = 'booked'
-        else:
-            qstatus = status[-1]['Status']
-            bookdate = status[0]['DateTime'][:10]
-        requests.get('https://skykingtrack.herokuapp.com/updateships', {
-            'tcode' : tcode,
-            'status' : qstatus,
-            'date' : bookdate
-        })
+    try:
+        dataJson = data.json()
+        for ships in dataJson:
+            tcode = ships['fields']['tcode']
+            url = f'https://live.skyking.co/api/Track/ConsignmentMTrack_WebSite_AuthNew?cnote={tcode}&key=skm&email=skm@flyking.co.in'
+            headers = {
+                "KeyValue": "wdFm5gZ7Wyh9aLvxcPU5Q1k5vhL/9K0g01d2H4JmJRR8+7VhRoucbv/SkTkRrXnk5R7PJkXm+ZoEtaXaR0Ci+MZ/gCektrIgalRYsorAspNDDISVSsm9qtJtfEraDbwf",
+                "Content-Type": "application/json"
+            }
+            res = requests.get(url, headers=headers)
+            status = res.json()
+            if(len(status) == 0):
+                qstatus = 'booked'
+            else:
+                qstatus = status[-1]['Status']
+                bookdate = status[0]['DateTime'][:10]
+            requests.get('https://skykingtrack.herokuapp.com/updateships', {
+                'tcode' : tcode,
+                'status' : qstatus,
+                'date' : bookdate
+            })
+    except:
+        print('error')
 
-# trackEveryShipment()
+
+while True:
+    trackEveryShipment()
+    time.sleep(3600)
         
